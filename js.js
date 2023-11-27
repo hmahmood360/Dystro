ccc6f4039d6c70152382e25f4175515fe34a7714
Hasan Mahmood <85750651+hmahmood360@users.noreply.github.com>
2023-11-27 23:36:45 +0500

    Web design for Dystro

---
+++ js.js
@@ -0,0 +1,40 @@
+function myFunction(){
+    var element = document.getElementById("minus");
+    element.classList.toggle("show");
+    
+
+    var element = document.getElementById("plus");
+    element.classList.toggle("remove");
+}
+function myFunction2(){
+    var element = document.getElementById("minus2");
+    element.classList.toggle("show");
+    
+
+    var element = document.getElementById("plus2");
+    element.classList.toggle("remove");
+}
+function myFunction3(){
+    var element = document.getElementById("minus3");
+    element.classList.toggle("show");
+    
+
+    var element = document.getElementById("plus3");
+    element.classList.toggle("remove");
+}
+function myFunction4(){
+    var element = document.getElementById("minus4");
+    element.classList.toggle("show");
+    
+
+    var element = document.getElementById("plus4");
+    element.classList.toggle("remove");
+}
+function myFunction5(){
+    var element = document.getElementById("minus5");
+    element.classList.toggle("show");
+    
+
+    var element = document.getElementById("plus5");
+    element.classList.toggle("remove");
+}
